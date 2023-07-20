d9f8c6f80cd7cbbac926e6db19270faf59143fd8
chpark <chak577@gmail.com>
2023-07-20 14:33:25 +0900

    3

--- console.cs
+++ console.cs
@@ -1,9 +1,19 @@
-class ft_console
+using System;
+
+public class ft_console
 {
-	static void main(string[] arv)
-	{
-		Console.WriteLine("문자를 입력하십시오");
-		string name = Console.ReadLine();
-		Console.WriteLine("입력된 문자 : " + name);
-	}
+    public void Main(string[] arv)
+    {
+        bool isTrue = true;
+        ConsoleKeyInfo keys;
+        while (isTrue)
+        {
+            keys = Console.ReadKey(true);
+            Console.WriteLine("문자를 입력하십시오");
+            string name = Console.ReadLine();
+            Console.WriteLine("입력된 문자 : " + name);
+            if (keys.Key == ConsoleKey.Escape)
+                isTrue = false;
+        }
+    }
 }
